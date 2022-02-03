8344033e3d8a8048ff77b5131590133f1908a02a
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-03 11:58:20 +0900

    create topbar

---
+++ blog/blog1/src/App.js
@@ -0,0 +1,12 @@
+import TopBar from "./topbar/TopBar";
+
+function App() {
+  return (
+    <>
+      {/* you cann't use multiple elemnets inside a comnponent it should be a parent div */}
+      <TopBar />
+    </>
+  );
+}
+
+export default App;
