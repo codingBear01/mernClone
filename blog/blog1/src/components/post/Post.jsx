041c5f7a8630f321f018b0be951d1bb5eb2bcaab
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-02-03 22:40:04 +0900

    post link

--- blog/blog1/src/components/post/Post.jsx
+++ blog/blog1/src/components/post/Post.jsx
@@ -1,46 +1,49 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
 export default function post() {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.unsplash.com/photo-1531564701487-f238224b7ce3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        alt=""
-      />
+      <Link className="link" to="/post/:postId">
+        <img
+          className="postImg"
+          src="https://images.unsplash.com/photo-1531564701487-f238224b7ce3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          alt=""
+        />
 
-      <div className="postInfo">
-        <div className="PostCats">
-          <span className="postCat">Programming</span>
-          <span className="postCat">Books</span>
-          <span className="postCat">Reviews</span>
-          <span className="postCat">Cooking</span>
+        <div className="postInfo">
+          <div className="PostCats">
+            <span className="postCat">Programming</span>
+            <span className="postCat">Books</span>
+            <span className="postCat">Reviews</span>
+            <span className="postCat">Cooking</span>
+          </div>
+
+          <span className="postTitle">THIS IS POSTTITLE!!!</span>
+
+          <span className="postDate">1 hour ago</span>
         </div>
 
-        <span className="postTitle">THIS IS POSTTITLE!!!</span>
-
-        <span className="postDate">1 hour ago</span>
-      </div>
-
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque
-        nihil sunt rem eveniet voluptatum commodi cupiditate quibusdam ratione
-        suscipit? Tenetur laboriosam libero ducimus exercitationem iure rem odit
-        cumque minima? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quasi itaque nihil sunt rem eveniet voluptatum commodi cupiditate
-        quibusdam ratione suscipit? Tenetur laboriosam libero ducimus
-        exercitationem iure rem odit cumque minima? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Quasi itaque nihil sunt rem eveniet
-        voluptatum commodi cupiditate quibusdam ratione suscipit? Tenetur
-        laboriosam libero ducimus exercitationem iure rem odit cumque minima?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque
-        nihil sunt rem eveniet voluptatum commodi cupiditate quibusdam ratione
-        suscipit? Tenetur laboriosam libero ducimus exercitationem iure rem odit
-        cumque minima? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quasi itaque nihil sunt rem eveniet voluptatum commodi cupiditate
-        quibusdam ratione suscipit? Tenetur laboriosam libero ducimus
-        exercitationem iure rem odit cumque minima?
-      </p>
+        <p className="postDesc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque
+          nihil sunt rem eveniet voluptatum commodi cupiditate quibusdam ratione
+          suscipit? Tenetur laboriosam libero ducimus exercitationem iure rem
+          odit cumque minima? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Quasi itaque nihil sunt rem eveniet voluptatum commodi
+          cupiditate quibusdam ratione suscipit? Tenetur laboriosam libero
+          ducimus exercitationem iure rem odit cumque minima? Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Quasi itaque nihil sunt rem
+          eveniet voluptatum commodi cupiditate quibusdam ratione suscipit?
+          Tenetur laboriosam libero ducimus exercitationem iure rem odit cumque
+          minima? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          itaque nihil sunt rem eveniet voluptatum commodi cupiditate quibusdam
+          ratione suscipit? Tenetur laboriosam libero ducimus exercitationem
+          iure rem odit cumque minima? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quasi itaque nihil sunt rem eveniet voluptatum
+          commodi cupiditate quibusdam ratione suscipit? Tenetur laboriosam
+          libero ducimus exercitationem iure rem odit cumque minima?
+        </p>
+      </Link>
     </div>
   );
 }
