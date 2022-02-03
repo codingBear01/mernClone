5dead51466ad6200274cc3c4ed749b9950aba1e9
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-03 18:11:00 +0900

    create settings-page

--- blog/blog1/src/App.js
+++ blog/blog1/src/App.js
@@ -2,13 +2,14 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import home from "./pages/home/Home";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       {/* you can't use multiple elemnets inside a comnponent it should be a parent div */}
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
