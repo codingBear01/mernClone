f732c3fd5c7cd17ff78054aa18154a2fff8dd9e8
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-03 18:38:48 +0900

    create pages-login, register

--- blog/blog1/src/App.js
+++ blog/blog1/src/App.js
@@ -3,13 +3,15 @@ import Single from "./pages/single/Single";
 import home from "./pages/home/Home";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <>
       {/* you can't use multiple elemnets inside a comnponent it should be a parent div */}
       <TopBar />
-      <Settings />
+      <Register />
     </>
   );
 }
---
+++ blog/blog1/src/pages/register/Register.jsx
@@ -0,0 +1,36 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Please Enter Your Name"
+        />
+
+        <label>Email</label>
+        <input
+          className="registerInput"
+          type="email"
+          placeholder="Please Enter Your Email"
+        />
+
+        <label>Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Please Enter Your Password"
+        />
+
+        <button className="registerButton">Register</button>
+      </form>
+
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
