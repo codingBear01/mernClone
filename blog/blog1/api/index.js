4b3160c11eea87e79d6e73a7070f77f2245b02a0
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-04 12:14:50 +0900

    create auth

--- blog/blog1/api/index.js
+++ blog/blog1/api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -20,6 +22,8 @@ await mongoose.connect('mongodb://localhost:27017/test', {
   useNewUrlParser: true, // <-- no longer necessary
   useUnifiedTopology: true // <-- no longer necessary
 });*/
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running!");
