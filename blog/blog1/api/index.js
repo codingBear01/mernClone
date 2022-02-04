d39af0b6c2068036024cc9eab92f59f9c25471b1
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-04 10:17:13 +0900

    basic mongoose settings

--- blog/blog1/api/index.js
+++ blog/blog1/api/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB!"))
+  .catch((err) => console.log(err));
+
+/*  No More Deprecation Warning Options
+useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
+
+// No longer necessary:
+mongoose.set('useFindAndModify', false);
+
+await mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true, // <-- no longer necessary
+  useUnifiedTopology: true // <-- no longer necessary
+});*/
 
 app.listen("5000", () => {
   console.log("Backend is running!");
