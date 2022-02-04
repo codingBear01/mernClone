d31b0d29d04521a36fd92fcb6c82c6b2a618da74
codingBear <95178119+codingbearda@users.noreply.github.com>
2022-02-04 10:53:08 +0900

    create models and define Schema

---
+++ blog/blog1/api/models/Category.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const CategoryUserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
